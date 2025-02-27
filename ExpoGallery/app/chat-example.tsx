27b7e7cbf7356197f30617fbe1becb4817f79d5a
Curt Cox <curtcox@gmail.com>
2025-02-27 13:45:29 -0600

    Added minimal demo for react-native-gifted-chat. See https://github.com/FaridSafi/react-native-gifted-chat

--- ExpoGallery/app/chat-example.tsx
+++ ExpoGallery/app/chat-example.tsx
@@ -1,7 +1,37 @@
-import { ThemedText } from '@/components/ThemedText';
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat, IMessage } from 'react-native-gifted-chat'
 
 export default function HomeScreen() {
+  const [messages, setMessages] = useState<IMessage[]>([])
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages: IMessage[] = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+  }, [])
+
   return (
-      <ThemedText type="title">Chat Example</ThemedText>
-  );
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
 }
