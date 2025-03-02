30aa84d459c4ce6368c38e1504de985e55b377fb
Curt Cox <curtcox@gmail.com>
2025-03-02 08:02:56 -0600

    Added example material icons.

--- ExpoGallery/app/icons-example.tsx
+++ ExpoGallery/app/icons-example.tsx
@@ -1,5 +1,5 @@
 import { ThemedText } from '@/components/ThemedText';
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import { StyleSheet, View } from 'react-native';
 import { Link } from 'expo-router';
 
@@ -8,23 +8,44 @@ export default function Example() {
       <View>
         <ThemedText type="title">Icons Example</ThemedText>
         <Link href='https://ionic.io/ionicons'>Iconicons</Link>
-        {icon('images-sharp')}
-        {icon('map-outline')}
-        {icon('pin-outline')}
-        {icon('cloud-download-outline')}
-        {icon('folder-outline')}
-        {icon('code-outline')}
-        {icon('open-outline')}
-        {icon('volume-high-outline')}
-        {icon('mic-outline')}
+        {ionIcon('images-sharp')}
+        {ionIcon('map-outline')}
+        {ionIcon('pin-outline')}
+        {ionIcon('cloud-download-outline')}
+        {ionIcon('folder-outline')}
+        {ionIcon('code-outline')}
+        {ionIcon('open-outline')}
+        {ionIcon('volume-high-outline')}
+        {ionIcon('mic-outline')}
+
+        <ThemedText type="title" style={styles.sectionTitle}>Material Icons</ThemedText>
+        <Link href='https://fonts.google.com/icons'>Material Icons</Link>
+        {materialIcon('home')}
+        {materialIcon('favorite')}
+        {materialIcon('settings')}
+        {materialIcon('search')}
+        {materialIcon('person')}
+        {materialIcon('shopping-cart')}
+        {materialIcon('add-shopping-cart')}
+        {materialIcon('shop')}
+        {materialIcon('shop-two')}
       </View>
   );
 }
 
-function icon(name: string) {
+function ionIcon(name: keyof typeof Ionicons.glyphMap) {
   return (
     <View style={styles.iconRow}>
       <Ionicons name={name} size={24} color="black" />
+      <ThemedText> {name}</ThemedText>
+    </View>
+  );
+}
+
+function materialIcon(name: keyof typeof MaterialIcons.glyphMap) {
+  return (
+    <View style={styles.iconRow}>
+      <MaterialIcons name={name} size={24} color="black" />
       <ThemedText> {name}</ThemedText>
     </View>
   );
@@ -35,5 +56,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     marginVertical: 8,
+  },
+  sectionTitle: {
+    marginTop: 20,
+    marginBottom: 10,
   },
 });
