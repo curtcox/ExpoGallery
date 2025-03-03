b84c2655df77b9a539a2766daf8103a6b7e20deb
Curt Cox <curtcox@gmail.com>
2025-03-02 22:22:58 -0600

    Add St. Louis flag for logo.

--- ExpoGallery/app/index_3.tsx
+++ ExpoGallery/app/index_3.tsx
@@ -11,7 +11,7 @@ export default function HomeScreen_3() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/logo.png')}
           style={styles.reactLogo}
         />
       }>
@@ -44,8 +44,8 @@ const styles = StyleSheet.create({
     marginVertical: 5,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 491,
+    width: 800,
     bottom: 0,
     left: 0,
     position: 'absolute',
