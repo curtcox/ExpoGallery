857961dc5af95fb1297df6a41166dfa81d96893a
Curt Cox <curtcox@gmail.com>
2025-03-04 17:09:24 -0600

    Add logo to image page.

--- ExpoGallery/app/image-example.tsx
+++ ExpoGallery/app/image-example.tsx
@@ -1,25 +1,25 @@
-import { Image, View } from 'react-native';
-
+import { Image, ScrollView, View } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
 export default function Example() {
 
   return (
-    <View>
+    <ScrollView>
+        <ThemedText>React Logos</ThemedText>
         <Image
           source={require('../assets/images/react-logo.png')}
-          style={{ width: 30, height: 30 }}
-        />
-        <Image
-          source={require('../assets/images/icon.png')}
           style={{ width: 30, height: 30 }}
         />
         <Image
           source={require('../assets/images/partial-react-logo.png')}
           style={{ width: 30, height: 30 }}
         />
-        <Image
-          source={require('../assets/images/splash-icon.png')}
-          style={{ width: 30, height: 30 }}
-        />
-    </View>
+
+        <ThemedText>Logos</ThemedText>
+        <Image source={require('@/assets/images/logo.png')} />
+
+        <ThemedText>Icons</ThemedText>
+        <Image source={require('../assets/images/icon.png')} />
+        <Image source={require('../assets/images/splash-icon.png')} />
+    </ScrollView>
   );
 }
