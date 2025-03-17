0de81972d1aaec19bbc4f68c96fbff852bd53e84
Curt Cox <curtcox@gmail.com>
2025-03-17 10:51:21 -0500

    Fix, skip, and delete various borken tests.

--- ExpoGallery/jest.config.js
+++ ExpoGallery/jest.config.js
@@ -7,6 +7,15 @@ const config = {
   ],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  // Ignore specific example files that aren't actual tests
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    'externalChatExample.ts',
+    'manualExample.js'
   ]
 };
 
