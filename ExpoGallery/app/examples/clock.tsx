5311812d7684ac4771c9ae409f7944e6c540350a
Curt Cox <curtcox@gmail.com>
2025-03-22 09:39:53 -0500

    Remove unused import.

--- ExpoGallery/app/examples/clock.tsx
+++ ExpoGallery/app/examples/clock.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ThemedText } from '@/components/ThemedText';
-import { info, error } from '@/utils/logger';
+import { info } from '@/utils/logger';
 
 export default function Example() {
   const [currentDateTime, setCurrentDateTime] = useState(new Date());
